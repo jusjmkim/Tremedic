c164e81f3bb05a31a3e428dd3b4006ece1e38832
jusjmkim <justinjmkim@gmail.com>
2014-09-21 00:07:33 -0400

    Add favicon

--- app.js
+++ app.js
@@ -152,7 +152,7 @@ function configureViews() {
   app.set('views', path.join(__dirname, 'views'));
 
   // uncomment after placing your favicon in /public
-  //app.use(favicon(__dirname + '/public/favicon.ico'));
+  app.use(favicon(__dirname + '/public/images/favicon.ico'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(cookieParser());
