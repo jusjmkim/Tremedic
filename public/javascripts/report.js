f5a0ecc253129e5be9e49a20af52ad59d1d1677e
Kenny Song <hello@kennysong.com>
2014-09-21 04:45:29 -0400

    First version history page

--- public/javascripts/report.js
+++ public/javascripts/report.js
@@ -17,132 +17,54 @@ function listenForPreviousData() {
   });
 }
 
-function listenToSendEmail() {
-  $("#email-button").click(function(event) {
-    event.preventDefault();
-
-    server.emit('sendEmail');
-  });
-}
-
-function listenToSendText() {
-  $("#text-button").click(function(event) {
-    event.preventDefault();
-
-    server.emit('sendText');
-  });
-}
-
 (function() {
   listenForStats();
   listenForPreviousData();
 })();
 
-$(function() {
-  listenToSendEmail();
-  listenToSendText();
-});
-
-/** Myo Functionality **/
-
-var gyroscopeData = {x: 0, time:0};
-var gyroscopeHistory = [];
-var dataInterval = 100; // milliseconds
-
-function getTime() {
-  var ms = new Date().getTime();
-  return ms / 1000;
-} 
-
-function sendDataToServer() {
-  server.emit('data', {'gyroscopeData': gyroscopeData});
-}
-
-Myo.start();
-
-Myo.on('imu', function(data){
-  // On new inertial measurement unit value
-  // Get current gyroscope & accelerometer values
-
-  gyroscopeData = {
-  x: data.gyroscope.x,
-  time: getTime()
-  };
-
-  // Save points to history
-  gyroscopeHistory.push(gyroscopeData);
-});
-
-// Send data to server every specified milliseconds
-setInterval(sendDataToServer, dataInterval);
-
 /** Chart Functionality **/
 
 $(function () {
-  Highcharts.setOptions({
-    global : {
-      useUTC : true
-    }
-  });
-
   // Create the chart
-  $('#chart').highcharts('StockChart', {
-    chart : {
-      events : {
-        load : function () {
-          // set up the updating of the chart each second
-          var series = this.series[0];
-          setInterval(function () {
-            var x = (new Date()).getTime();
-            var y = gyroscopeData.x;
-            series.addPoint([x, y], true, true);
-          }, 100);
-        }
-      }
-    },
+  $('#chart-frequency').highcharts('StockChart', {
 
-    rangeSelector: {
-      buttons: [{
-        count: 10,
-        type: 'second',
-        text: '10s'
-      }, {
-        count: 30,
-        type: 'second',
-        text: '30s'
-      }, {
-        type: 'all',
-        text: 'All'
-      }],
-      inputEnabled: false,
-      selected: 0
-    },
+      rangeSelector: {
+          inputEnabled: $('#chart-frequency').width() > 480,
+          selected: 1
+      },
 
-    title : {
-      text : 'Current Tremor Magnitude (°/sec)'
-    },
+      title: {
+          text: 'Mean Tremor Frequency (Hz)'
+      },
 
-    exporting: {
-      enabled: false
-    },
-
-    series : [{
-      name : 'Tremor Magnitude',
-      data : (function () {
-        // generate an array of random data
-        var data = [];
-        var time = (new Date()).getTime();
-
-        for (var i = -999; i <= 0; i += 1) {
-          data.push([
-            time + i * 1000,
-            Math.round(Math.random() * 300)
-          ]);
-        }
-
-        return data;
-      }()),
-      type: 'spline'
-    }]
+      series: [{
+          name: 'Tremor Frequency',
+          data: [],
+          type: 'spline',
+      }]
   });
+});
+
+$(function () {
+    $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=msft-c.json&callback=?', function (data) {
+
+        // Create the chart
+        $('#chart-magnitude').highcharts('StockChart', {
+
+            rangeSelector: {
+                inputEnabled: $('#chart-magnitude').width() > 480,
+                selected: 1
+            },
+
+            title: {
+                text: 'Mean Tremor Magnitude (°/sec)'
+            },
+
+            series: [{
+                name: 'Tremor Magnitude',
+                data: data,
+                type: 'spline',
+            }]
+        });
+    });
 });
